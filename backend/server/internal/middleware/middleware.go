20550f8503d27c61b7a603c6ae71f807436c5907
Nikita Ukhin <nvukhin@avito.ru>
2023-05-31 23:57:48 +0300

    fix cross-origin

--- backend/server/internal/middleware/middleware.go
+++ backend/server/internal/middleware/middleware.go
@@ -99,7 +99,7 @@ func WsMiddleware(h appWsHandler) authHandler {
 func CorsMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Token, Accept, Origin, chatId")
